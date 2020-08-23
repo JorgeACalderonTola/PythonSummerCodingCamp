1887ca8d7d5991e51b577f91c79212bb007f73e8
Jorge A. Calderon Tola <67562342+JorgeCalderonTola@users.noreply.github.com>
2020-08-22 23:43:47 -0400

    Creado con Colaboratory

---
+++ HolaMundo.py
@@ -0,0 +1,53 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "Untitled0.ipynb",
+      "provenance": [],
+      "authorship_tag": "ABX9TyMVuzsKtfdDqcd1axIYL5Xy",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/JorgeCalderonTola/PythonSummerCodingCamp/blob/master/HolaMundo.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "yqKqXrzbZ_E9",
+        "colab_type": "code",
+        "colab": {
+          "base_uri": "https://localhost:8080/",
+          "height": 34
+        },
+        "outputId": "52a7d863-bcb4-4ed0-a0f3-3db181aa8eb6"
+      },
+      "source": [
+        "print(\"24\")"
+      ],
+      "execution_count": 2,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "text": [
+            "24\n"
+          ],
+          "name": "stdout"
+        }
+      ]
+    }
+  ]
+}
